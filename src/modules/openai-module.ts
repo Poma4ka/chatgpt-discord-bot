8005800d19cb0ff9463c9f564ae3e5ab460cdc10
Roman Glushinskiy <roman21113007@gmail.com>
2023-04-30 04:57:12 +0300

    init

--- src/modules/openai-module.ts
+++ src/modules/openai-module.ts
@@ -93,7 +93,7 @@ export default class {
                             history,
                             message,
                             options,
-                            attempt++,
+                            ++attempt,
                         ),
                     () => ({
                         status: false,
